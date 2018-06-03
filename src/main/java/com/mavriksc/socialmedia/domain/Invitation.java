23a62b5352938978a7474dd74addf031e6e8c003
scarlisle <s.m.carlisle@gmail.com>
2018-06-03 09:45:37 -0500

    add Invitation

---
+++ src/main/java/com/mavriksc/socialmedia/domain/Invitation.java
@@ -0,0 +1,26 @@
+package com.mavriksc.socialmedia.domain;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Invitation {
+    @Id
+    private Long id;
+    private UUID code;
+    private Date expiration;
+    private Boolean used;
+
+    public Invitation(){
+        code = UUID.randomUUID();
+        expiration  = Calendar.getInstance().getTime();
+        used = false;
+    }
+}
